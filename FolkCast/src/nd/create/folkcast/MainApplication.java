6aea085c2d817b1a8f3ffece7a0f0889819ab67c
NPD <niall.p.deehan@gmail.com>
2015-01-30 15:22:40 +0100

    Got basic GUI working

--- FolkCast/src/nd/create/folkcast/MainApplication.java
+++ FolkCast/src/nd/create/folkcast/MainApplication.java
@@ -1,23 +1,107 @@
 package nd.create.folkcast;
 
+import java.io.IOException;
+
+import nd.create.folkcast.model.Feed;
+import nd.create.folkcast.view.PodcastListViewController;
+import nd.create.folkcast.view.RootLayoutController;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application { 
+	
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    
+    private RootLayoutController rootLayoutController;
+    private PodcastListViewController podcastListViewController;
+    
+	// This will hold all of the Feeds   
+    private ObservableList<Feed> rssFeeds = FXCollections.observableArrayList();
+  
 
 	public MainApplication() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    /*
+     * Launches the application
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+
+		   this.primaryStage = primaryStage;
+	       this.primaryStage.setTitle("Folk Cast");
+	      // this.primaryStage.getIcons().add(new Image("file:resources/images/SOMETHING.png"));
+
+	       initRootLayout();
+	       
+	       initPodcastListView();
+
 		
 	}
+
+	private void initPodcastListView() {
+    	try {
+            // Load podcast overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApplication.class.getResource("view/PodcastListView.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Set podcast overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+
+            // Give the controller access to the main app.
+            PodcastListViewController controller = loader.getController();
+            controller.setMainApp(this);
+            podcastListViewController = controller;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	
+		
+	}
+
+	private void initRootLayout() {
+		 try {
+	            // Load root layout from fxml file.
+	            FXMLLoader loader = new FXMLLoader();
+	            loader.setLocation(MainApplication.class
+	                    .getResource("view/RootLayout.fxml"));
+	            rootLayout = (BorderPane) loader.load();
+
+	            // Show the scene containing the root layout.
+	            Scene scene = new Scene(rootLayout);
+	            primaryStage.setScene(scene);
+
+	            // Give the controller access to the main app.
+	            RootLayoutController controller = loader.getController();
+	            controller.setMainApp(this);
+	            rootLayoutController = controller;
+
+	            primaryStage.show();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		
+	}
+	
+    @FXML
+    private void handelClose(){
+    	System.exit(0);
+    }
+
 
 }
--- FolkCast/src/nd/create/folkcast/view/RootLayoutController.java
+++ FolkCast/src/nd/create/folkcast/view/RootLayoutController.java
@@ -1,9 +1,23 @@
 package nd.create.folkcast.view;
 
+import nd.create.folkcast.MainApplication;
+
+
 public class RootLayoutController {
+	
+	MainApplication mainApp;
 
 	public RootLayoutController() {
 		// TODO Auto-generated constructor stub
 	}
+	
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(MainApplication mainApp) {
+        this.mainApp = mainApp;
+    }
 
 }
